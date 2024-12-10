2b0fd72ea065fb50dbfc458a2c9aba3cacda83e3
Carlos Guerrero <carlosguerrero0705@gmail.com>
2024-12-10 07:42:00 -0500

    fix: modify envs to livekit

--- src/common/envs.ts
+++ src/common/envs.ts
@@ -6,6 +6,9 @@ interface EnvVars {
   PORT: number;
   ORIGIN_CORS: string;
   DATABASE_URL: string;
+  LIVEKIT_API_KEY: string;
+  LIVEKIT_API_SECRET: string;
+  LIVEKIT_SERVER_URL: string;
 }
 
 const envsSchema = joi
@@ -13,6 +16,9 @@ const envsSchema = joi
     PORT: joi.number().required(),
     ORIGIN_CORS: joi.string().required(),
     DATABASE_URL: joi.string().required(),
+    LIVEKIT_API_KEY: joi.string().required(),
+    LIVEKIT_API_SECRET: joi.string().required(),
+    LIVEKIT_SERVER_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -20,6 +26,9 @@ const { error, value } = envsSchema.validate({
   ...process.env,
   ORIGIN_CORS: process.env.ORIGIN_CORS,
   DATABASE_URL: process.env.DATABASE_URL,
+  LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
+  LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
+  LIVEKIT_SERVER_URL: process.env.LIVEKIT_SERVER_URL,
 });
 
 if (error) {
@@ -32,4 +41,7 @@ export const envs = {
   PORT: envVars.PORT,
   ORIGIN_CORS: envVars.ORIGIN_CORS,
   DATABASE_URL: envVars.DATABASE_URL,
+  LIVEKIT_API_KEY: envVars.LIVEKIT_API_KEY,
+  LIVEKIT_API_SECRET: envVars.LIVEKIT_API_SECRET,
+  LIVEKIT_SERVER_URL: envVars.LIVEKIT_SERVER_URL,
 };
