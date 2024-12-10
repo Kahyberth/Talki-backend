e879ee2ccc48fd98639517a9ca6d702d94841570
Carlos Guerrero <carlosguerrero0705@gmail.com>
2024-12-10 07:41:44 -0500

    fix: modify cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { envs } from './common/envs';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: envs.ORIGIN_CORS, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(envs.PORT).then(() => logger.log(`Application listening on port ${envs.PORT}`));
-  
+
 }
 bootstrap();
